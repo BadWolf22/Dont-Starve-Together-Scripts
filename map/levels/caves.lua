efb4727c850aa6175f5e76c461b80805eb018a51
BadWolf22 <matthewpostel02@gmail.com>
2021-09-21 12:58:26 -0500

    added current version's scripts

---
+++ map/levels/caves.lua
@@ -0,0 +1,85 @@
+local dst_cave = {
+	id="DST_CAVE",
+	name=STRINGS.UI.CUSTOMIZATIONSCREEN.PRESETLEVELS.DST_CAVE,
+	desc= STRINGS.UI.CUSTOMIZATIONSCREEN.PRESETLEVELDESC.DST_CAVE,
+	location = "cave",
+	version = 4,
+	overrides={
+	},
+	background_node_range = {0,1},
+}
+if IsConsole() then
+	dst_cave.name = STRINGS.UI.CUSTOMIZATIONSCREEN.PRESETLEVELS.DST_CAVE_PS4
+end
+AddLevel(LEVELTYPE.SURVIVAL, dst_cave)
+
+AddLevel(LEVELTYPE.SURVIVAL, {
+    id="DST_CAVE_PLUS",
+    name=STRINGS.UI.CUSTOMIZATIONSCREEN.PRESETLEVELS.DST_CAVE_PLUS,
+    desc= STRINGS.UI.CUSTOMIZATIONSCREEN.PRESETLEVELDESC.DST_CAVE_PLUS,
+    location = "cave",
+    version = 4,
+
+    overrides={
+        boons = "often",
+
+        cave_spiders = "often",
+
+        rabbits = "rare",
+
+        berrybush = "rare",
+        carrot = "rare",
+
+        flower_cave = "rare",
+        wormlights = "rare",
+    },
+    background_node_range = {0,1},
+})
+
+-- older depreciated AddLevel up here --
+
+AddWorldGenLevel(LEVELTYPE.SURVIVAL, {id="DST_CAVE",
+    name=STRINGS.UI.CUSTOMIZATIONSCREEN.PRESETLEVELS.DST_CAVE,
+    desc= STRINGS.UI.CUSTOMIZATIONSCREEN.PRESETLEVELDESC.DST_CAVE,
+    location = "cave",
+    version = 4,
+    overrides={},
+    background_node_range = {0,1},
+})
+AddSettingsPreset(LEVELTYPE.SURVIVAL, {id="DST_CAVE",
+    name=STRINGS.UI.CUSTOMIZATIONSCREEN.PRESETLEVELS.DST_CAVE,
+    desc= STRINGS.UI.CUSTOMIZATIONSCREEN.PRESETLEVELDESC.DST_CAVE,
+    location = "cave",
+    version = 4,
+    overrides={},
+})
+
+AddWorldGenLevel(LEVELTYPE.SURVIVAL, {id="DST_CAVE_PLUS",
+    name=STRINGS.UI.CUSTOMIZATIONSCREEN.PRESETLEVELS.DST_CAVE_PLUS,
+    desc= STRINGS.UI.CUSTOMIZATIONSCREEN.PRESETLEVELDESC.DST_CAVE_PLUS,
+    location = "cave",
+    version = 4,
+    overrides={
+        boons = "often",
+
+        cave_spiders = "often",
+
+        rabbits = "rare",
+
+        berrybush = "rare",
+        carrot = "rare",
+
+        flower_cave = "rare",
+        wormlights = "rare",
+    },
+    background_node_range = {0,1},
+})
+AddSettingsPreset(LEVELTYPE.SURVIVAL, {id="DST_CAVE_PLUS",
+    name=STRINGS.UI.CUSTOMIZATIONSCREEN.PRESETLEVELS.DST_CAVE_PLUS,
+    desc= STRINGS.UI.CUSTOMIZATIONSCREEN.PRESETLEVELDESC.DST_CAVE_PLUS,
+    location = "cave",
+    version = 4,
+    overrides={
+        flower_cave_regrowth = "rare",
+    },
+})
---
+++ prefabs/crabking_spawner.lua
@@ -0,0 +1,66 @@
+local prefabs =
+{
+    "crabking",
+}
+
+local CRABKING_SPAWNTIMER = "regen_crabking"
+
+local ZERO = Vector3(0,0,0)
+local function zero_spawn_offset(inst)
+    if TheWorld.Map:GetPlatformAtPoint(inst.Transform:GetWorldPosition()) then return end
+    return ZERO
+end
+
+local function OnKilled(inst)
+    inst.components.worldsettingstimer:StartTimer(CRABKING_SPAWNTIMER, TUNING.CRABKING_RESPAWN_TIME)
+end
+
+local function GenerateNewKing(inst)
+    inst.components.childspawner:AddChildrenInside(1)
+    inst.components.childspawner:StartSpawning()
+end
+
+local function ontimerdone(inst, data)
+    if data.name == CRABKING_SPAWNTIMER then
+        GenerateNewKing(inst)
+    end
+end
+
+local function OnPreLoad(inst, data)
+    if data and data.childspawner then
+        data.childspawner.spawning = true
+    end
+end
+
+local function fn()
+    local inst = CreateEntity()
+
+    inst.entity:AddTransform()
+    --[[Non-networked entity]]
+
+    inst:AddTag("CLASSIFIED")
+    inst:AddTag("crabking_spawner")
+
+    inst:AddComponent("childspawner")
+    inst.components.childspawner.childname = "crabking"
+    inst.components.childspawner.spawnoffscreen = true
+    inst.components.childspawner:SetMaxChildren(1)
+    inst.components.childspawner:SetSpawnPeriod(TUNING.CRABKING_SPAWN_TIME, 0)
+    inst.components.childspawner.onchildkilledfn = OnKilled
+    if not TUNING.SPAWN_CRABKING then
+        inst.components.childspawner.childreninside = 0
+    end
+    inst.components.childspawner:StartSpawning()
+    inst.components.childspawner:StopRegen()
+    inst.components.childspawner.overridespawnlocation = zero_spawn_offset
+
+    inst:AddComponent("worldsettingstimer")
+    inst.components.worldsettingstimer:AddTimer(CRABKING_SPAWNTIMER, TUNING.CRABKING_RESPAWN_TIME, TUNING.SPAWN_CRABKING)
+    inst:ListenForEvent("timerdone", ontimerdone)
+
+    inst.OnPreLoad = OnPreLoad
+
+    return inst
+end
+
+return Prefab("crabking_spawner", fn, nil, prefabs)
