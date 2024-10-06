28579182fdd710ed5ed860e4cc99e2ce25e2f20c
BadWolf22 <40918920+BadWolf22@users.noreply.github.com>
2024-10-05 21:20:42 -0500

    fix the file path

--- .github/workflows/update.sh
+++ .github/workflows/update.sh
@@ -2,8 +2,8 @@ cd ..
 wget https://github.com/SteamRE/DepotDownloader/releases/download/DepotDownloader_2.7.2/DepotDownloader-linux-x64.zip
 unzip ./DepotDownloader-linux-x64.zip -d depotDownloader
 chmod u+x ./depotDownloader/DepotDownloader
-./depotDownloader/DepotDownloader -app 343050 -filelist ./Testbed/.github/workflows/files.txt
+./depotDownloader/DepotDownloader -app 343050 -filelist ./Dont-Starve-Together-Scripts/.github/workflows/files.txt
 find -name "scripts.zip" -type f -exec mv -- {} . \;
 unzip ./scripts.zip
-rm -rf ./Testbed/*
-mv -v ./scripts/* ./Testbed
+rm -rf ./Dont-Starve-Together-Scripts/Dont-Starve-Together-Scripts/*
+mv -v ./scripts/* ./Dont-Starve-Together-Scripts/Dont-Starve-Together-Scripts
