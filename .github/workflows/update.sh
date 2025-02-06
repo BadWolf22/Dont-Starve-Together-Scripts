9a21a34baec38d683e1d232e1d0575d3284b0fea
BadWolf22 <40918920+BadWolf22@users.noreply.github.com>
2025-02-05 18:08:27 -0600

    Update update.sh

--- .github/workflows/update.sh
+++ .github/workflows/update.sh
@@ -1,5 +1,5 @@
 cd ..
-wget https://github.com/SteamRE/DepotDownloader/releases/download/DepotDownloader_2.7.2/DepotDownloader-linux-x64.zip
+wget https://github.com/SteamRE/DepotDownloader/releases/download/DepotDownloader_3.0.0/DepotDownloader-linux-x64.zip
 unzip ./DepotDownloader-linux-x64.zip -d depotDownloader
 chmod u+x ./depotDownloader/DepotDownloader
 ./depotDownloader/DepotDownloader -app 343050 -filelist ./Dont-Starve-Together-Scripts/.github/workflows/files.txt
